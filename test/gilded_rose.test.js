6ec8c8be3db17e93f32c5dd9747b645cba2ca3e3
shazwi <supershazwi@gmail.com>
2023-01-26 13:33:23 +0800

    Add test cases for backstage pass

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -22,18 +22,25 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toBe(42);
   });
 
-  // to test for items that are not aged brie, backstage passes or sulfuras
-  it("normal item quality should decrease normally if sellIn is above 0", function() {
-    const gildedRose = new Shop([new Item("Normal Item", 10, 40)]);
+  // to test for backstage passes
+  it('back pass quality should increase by 2 when sellIn is 10 days or less', function () {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 40)]);
     const items = gildedRose.updateQuality();
 
-    expect(items[0].quality).toBe(39);
+    expect(items[0].quality).toBe(42);
   });
 
-  it("normal item quality should decrease even more if sellIn falls below 0", function() {
-    const gildedRose = new Shop([new Item("Normal Item", 0, 40)]);
+  it('back pass quality should increase by 3 when sellIn is 5 days or less', function () {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 40)]);
     const items = gildedRose.updateQuality();
 
-    expect(items[0].quality).toBe(38);
+    expect(items[0].quality).toBe(43);
+  });
+
+  it('backstage pass quality should update to 0 if sellIn is 0', function () {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 40)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(0);
   });
 });
