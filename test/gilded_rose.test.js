80c8656696c05f0632ff675b24b208f0d8261d11
shazwi <supershazwi@gmail.com>
2023-01-26 13:25:58 +0800

    Add test cases for aged brie

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -4,6 +4,21 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+  });
+
+  // to test for aged brie
+  it("aged brie quality should increase normally if sellIn is above 0", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 30, 40)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(41);
+  });
+
+  it("aged brie quality should increase even more if sellIn falls below 0", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 40)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(42);
   });
 });
