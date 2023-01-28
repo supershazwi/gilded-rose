565583a35fce338c28a4d4ff5bc0082605a77f2b
Shazwi Suwandi <shazwisuwandi@Shazwis-MacBook-Air.local>
2023-01-28 10:05:15 +0800

    Add arrays to store respective items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,6 +10,11 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
+  static valueIncreaseItemNames = ['Aged Brie'];
+  static expireItemNames = ['Backstage passes to a TAFKAL80ETC concert'];
+  static legendaryItemNames = ['Sulfuras, Hand of Ragnaros'];
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
